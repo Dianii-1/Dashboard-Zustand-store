810c273f12a3185680b3eceab5ee4291fc3065be
Dianii-1 <dianii230416@gmail.com>
2026-01-29 21:47:21 -0500

    refactor: Send tasks by status

--- src/pages/02-objects/JiraPage.tsx
+++ src/pages/02-objects/JiraPage.tsx
@@ -4,13 +4,16 @@ import { useTaskStore } from "../../stores";
 export const JiraPage = () => {
   const tasks = useTaskStore((state) => state.tasks);
 
-  const pending = Object.values(tasks).filter((task) => task.status === "open");
-  const ready = Object.values(tasks).filter((task) => task.status === "done");
-  const inProgress = Object.values(tasks).filter(
+  const pendingTasks = Object.values(tasks).filter(
+    (task) => task.status === "open"
+  );
+  const DoneTasks = Object.values(tasks).filter(
+    (task) => task.status === "done"
+  );
+  const inProgressTasks = Object.values(tasks).filter(
     (task) => task.status === "in-progress"
   );
 
-  console.log({ pending, ready, inProgress }, { ready });
   return (
     <>
       <h1>Tareas</h1>
@@ -18,11 +21,15 @@ export const JiraPage = () => {
       <hr />
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <JiraTasks title="Pendientes" value="pending" />
+        <JiraTasks tasks={pendingTasks} title="Pendientes" value="open" />
 
-        <JiraTasks title="Avanzando" value="in-progress" />
+        <JiraTasks
+          tasks={inProgressTasks}
+          title="Avanzando"
+          value="in-progress"
+        />
 
-        <JiraTasks title="Terminadas" value="done" />
+        <JiraTasks tasks={DoneTasks} title="Terminadas" value="done" />
       </div>
     </>
   );
