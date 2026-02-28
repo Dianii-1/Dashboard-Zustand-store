4d9da54cbd03e8a4114c8240b0373c1602675a82
Dianii-1 <dianii230416@gmail.com>
2026-02-28 12:04:54 -0500

    refactor: Add dateslice in store

--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -2,12 +2,14 @@ import { create } from "zustand";
 import { createPersonSlice, PersonSlice } from "./person.slice";
 import { devtools } from "zustand/middleware";
 import { createGuestSlice, GuestSlice } from "./guest.slice";
+import { createDateSlice, DateSlice } from "./date.slice";
 
-type BoundState = PersonSlice & GuestSlice;
+type BoundState = PersonSlice & GuestSlice & DateSlice;
 
 export const useWeddingBoundStore = create<BoundState>()(
   devtools((...a) => ({
     ...createPersonSlice(...a),
     ...createGuestSlice(...a),
+    ...createDateSlice(...a),
   }))
 );
