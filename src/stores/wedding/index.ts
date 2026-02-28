fc64ca8121300ad4b329b9764ef0c4284f942747
Dianii-1 <dianii230416@gmail.com>
2026-02-28 11:32:11 -0500

    refactor: Add gest slice in bound store

--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -1,11 +1,13 @@
 import { create } from "zustand";
 import { createPersonSlice, PersonSlice } from "./person.slice";
 import { devtools } from "zustand/middleware";
+import { createGuestSlice, GuestSlice } from "./guest.slice";
 
-type BoundState = PersonSlice;
+type BoundState = PersonSlice & GuestSlice;
 
 export const useWeddingBoundStore = create<BoundState>()(
   devtools((...a) => ({
     ...createPersonSlice(...a),
+    ...createGuestSlice(...a),
   }))
 );
