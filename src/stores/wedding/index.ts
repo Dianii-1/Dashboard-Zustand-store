75a0c8e5251d6026d56fe8354b8a1cae60a4f323
Dianii-1 <dianii230416@gmail.com>
2026-02-28 11:08:18 -0500

    feat: Add devtools

--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -1,8 +1,11 @@
 import { create } from "zustand";
 import { createPersonSlice, PersonSlice } from "./person.slice";
+import { devtools } from "zustand/middleware";
 
 type BoundState = PersonSlice;
 
-export const useWeddingBoundStore = create<BoundState>()((...a) => ({
-  ...createPersonSlice(...a),
-}));
+export const useWeddingBoundStore = create<BoundState>()(
+  devtools((...a) => ({
+    ...createPersonSlice(...a),
+  }))
+);
