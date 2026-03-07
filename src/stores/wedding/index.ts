6cb4d58d1afb9bd6a89f9bc9799807da319e771c
Dianii-1 <dianii230416@gmail.com>
2026-03-07 11:36:55 -0500

    feat: create conffirmed slice

--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -3,13 +3,18 @@ import { createPersonSlice, PersonSlice } from "./person.slice";
 import { devtools } from "zustand/middleware";
 import { createGuestSlice, GuestSlice } from "./guest.slice";
 import { createDateSlice, DateSlice } from "./date.slice";
+import {
+  ConfirmationSlice,
+  createConfirmationSlice,
+} from "./confirmation.slice";
 
-type BoundState = PersonSlice & GuestSlice & DateSlice;
+type BoundState = PersonSlice & GuestSlice & DateSlice & ConfirmationSlice;
 
 export const useWeddingBoundStore = create<BoundState>()(
   devtools((...a) => ({
     ...createPersonSlice(...a),
     ...createGuestSlice(...a),
     ...createDateSlice(...a),
+    ...createConfirmationSlice(...a),
   }))
 );
